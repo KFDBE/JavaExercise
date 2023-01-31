d6780bc473fdf826248814196119131a8c526622
KFDBES <asignkfdbes_1@qq.com>
2023-01-31 21:11:54 +0800

    第二讲完

--- exercise01/src/exercise/variable.java
+++ exercise01/src/exercise/variable.java
@@ -1,13 +1,41 @@
 package exercise;
 
 public class variable {
+
     public static void main(String[] args){
 
         //int整形
         int a = 1;
         System.out.println(a);
 
+        //byte字节型
+        byte b = 127;//注：这里的范围需要注意哦
+        System.out.println(b);
 
+        //short短整型
+        short c = 200;
+        System.out.println(c);
 
+        //long长整型
+        long d = 1000;
+        System.out.println(d);
+
+        //float单精度浮点型
+        float e = (float) 1.2;//強制类型转换
+        System.out.println(e);
+
+        //double双精度浮点型
+        double f = 1.2;
+        System.out.println(f);
+
+        //char字符型
+        char h = 's';
+        System.out.println(h);
+
+        //boolean布尔类型
+        boolean i = true;
+        System.out.println(i);
+
+                 
     }
 }
