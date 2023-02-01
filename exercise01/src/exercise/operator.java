6ef433052a2a4f106335ddd682f2c1c4ca8b0a17
KFDBES <asignkfdbes_1@qq.com>
2023-02-01 15:40:55 +0800

    运算符

---
+++ exercise01/src/exercise/operator.java
@@ -0,0 +1,20 @@
+package exercise;
+
+public class operator {
+    public static void main(String[] args){
+
+        int a = 1;
+        int b = 2;
+
+        //运算符
+        int c =  a + b;
+        int d =  a - b;
+        int e =  a * b;
+        double f = a / b;
+        System.out.println(c);
+        System.out.println(e);
+        System.out.println(d);
+        System.out.println(f);
+
+    }
+}
