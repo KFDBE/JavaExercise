a97b6dbdd7a550dab531f073e2543c86c46ea429
KFDBES <asignkfdbes_1@qq.com>
2023-02-04 19:39:44 +0800

    运算符,逻辑与逻辑或

--- exercise01/src/exercise/operator.java
+++ exercise01/src/exercise/operator.java
@@ -16,5 +16,31 @@ public class operator {
         System.out.println(d);
         System.out.println(f);
 
+        //关系运算符
+        int a1 = 3;
+        int b1 = 2;
+
+        boolean c1 = ( a1 > b1 );
+        System.out.println(c1);
+
+        //逻辑运算符
+        int a2 = 1;
+        int b2 = 2;
+        int c2 = 3;
+        int d2 = 4;
+        int f2 = 5;
+
+        boolean e2  = ( a2 < b2 ) && ( c2 < d2 );
+        System.out.println(e2);
+
+        boolean e3 = (a2<b2)||(c2<d2);
+        System.out.println(e3);
+
+        boolean e4 = (a > b)||((f = c) < d);
+        System.out.println(e4);
+        System.out.println(f2);
+
+
+
     }
 }
