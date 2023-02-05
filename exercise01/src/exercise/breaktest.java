146d0445fbcbc9382a4a09616e98d5ef9a12a13e
KFDBES <asignkfdbes_1@qq.com>
2023-02-05 12:26:50 +0800

    流程控制语句break continue

---
+++ exercise01/src/exercise/breaktest.java
@@ -0,0 +1,12 @@
+package exercise;
+
+public class breaktest {
+    public static void main(String[] args){
+        for (int i = 0; i < 10 ;i++) {
+            if ( 5 == i){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
